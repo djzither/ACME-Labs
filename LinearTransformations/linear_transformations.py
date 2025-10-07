aa81d3fae8d7b494767a4feea8432a1d090311ae
Derek Robinson <dj.zither@gmail.com>
2025-10-07 16:42:14 +0000

    done and fixed

--- LinearTransformations/linear_transformations.py
+++ LinearTransformations/linear_transformations.py
@@ -10,6 +10,7 @@ import numpy as np
 from random import random
 from matplotlib import pyplot as plt
 
+
 # Problem 1
 def stretch(A, a, b):
     """Scale the points in A by a in the x direction and b in the
@@ -22,7 +23,9 @@ def stretch(A, a, b):
     Return:
         ((2,n) ndarray): Transformed matrix
     """
-    raise NotImplementedError("Problem 1 Incomplete")
+    #stretch matrix
+    S = np.array([[a, 0], [0, b]])
+    return S @ A
 
 def shear(A, a, b):
     """Slant the points in A by a in the x direction and b in the
@@ -35,7 +38,10 @@ def shear(A, a, b):
     Return:
         ((2,n) ndarray): Transformed matrix
     """
-    raise NotImplementedError("Problem 1 Incomplete")
+    #shear matrix
+    S = np.array([[1, a], [b, 1]])
+    return A.T @ S 
+
 
 def reflect(A, a, b):
     """Reflect the points in A about the line that passes through the origin
@@ -48,7 +54,10 @@ def reflect(A, a, b):
     Return:
         ((2,n) ndarray): Transformed matrix
     """
-    raise NotImplementedError("Problem 1 Incomplete")
+    #reflect matrix
+    scaler = 1/(a**2 + b**2)
+    R = ([[(a**2 - b**2), (2 * a * b)], [(2 * a * b), (b**2 - a**2)]])
+    return R @ scaler @ A
 
 def rotate(A, theta):
     """Rotate the points in A about the origin by theta radians.
@@ -59,10 +68,15 @@ def rotate(A, theta):
     Return:
         ((2,n) ndarray): Transformed matrix
     """
-    raise NotImplementedError("Problem 1 Incomplete")
+    #rotate matrix
+    return np.array([
+        [np.cos(theta), -np.sin(theta)],
+        [np.sin(theta),  np.cos(theta)]
+    ]) @ A
 
 
 # Problem 2
+
 def solar_system(T, x_e, x_m, omega_e, omega_m):
     """Plot the trajectories of the earth and moon over the time interval [0,T]
     assuming the initial position of the earth is (x_e,0) and the initial
@@ -75,7 +89,40 @@ def solar_system(T, x_e, x_m, omega_e, omega_m):
         omega_e (float): The earth's angular velocity.
         omega_m (float): The moon's angular velocity.
     """
-    raise NotImplementedError("Problem 2 Incomplete")
+    times = np.linspace(0, T, 1000)
+    #get origional places
+    p_e0 = [x_e, 0]
+    p_m0 = [x_m, 0]
+    rel_m0 = [p_m0[0] - p_e0[0], p_m0[1] - p_e0[1]]
+    #to store x and y coordinates
+    earth_x, earth_y = [], []
+    moon_x, moon_y = [], []
+
+    for t in times:
+        #clockwise
+        p_e = rotate(p_e0, omega_e * t)
+        
+        #pos moon cntr clockwise
+        rel_m = rotate(rel_m0, omega_m * t)
+
+        #have to translate
+        p_m = [p_e[0] - rel_m[0], p_e[1] - rel_m[1]]
+
+
+        earth_x.append(p_e[0])
+        earth_y.append(p_e[1])
+        moon_x.append(p_m[0])
+        moon_y.append(p_m[1])
+    
+    #plot
+    fig, ax = plt.subplots(figsize=(6, 6))
+    ax.plot(earth_x, earth_y, label="Earth Orbit")
+    ax.plot(moon_x, moon_y, label="Moon Orbit")
+    ax.scatter([0], [0], c="yellow", s=200, label="Sun")
+    ax.set_aspect("equal")
+    ax.legend()
+    
+    plt.savefig("solar_system.png", dpi=300)
 
 
 def random_vector(n):
@@ -111,8 +158,38 @@ def prob3():
     that your figure accurately describes the growth, but avoid values of n
     that lead to execution times of more than 1 minute.
     """
-    raise NotImplementedError("Problem 3 Incomplete")
+    # gonna time the times
+    ns = [50, 100, 150, 200, 250]
+    vec_times = []
+    mat_times = []
+    for n in ns:
+        A = random_matrix(n)
+        x = random_vector(n)
+        B = random_matrix(n)
 
+        start = time.time()
+        matrix_vector_product(A, x)
+        vec_time = (time.time() - start)
+        vec_times.append(vec_time)
+       
+        start_mat = time.time()
+        matrix_matrix_product(A, B)
+        mat_time = (time.time() - start_mat)
+        mat_times.append(mat_time)
+    #plot the results
+    fig, axs = plt.subplots(1, 2)
+    axs[0].plot(ns, vec_times, marker='o')
+    axs[0].set_title("Matrix Vector Product Timing")
+    axs[0].set_xlabel("matrix size")
+    axs[0].set_ylabel("Time (s)")
+
+    axs[1].plot(ns, mat_times, marker='o', color='orange')
+    axs[1].set_title("Matrix Matrix Product Timing")
+    axs[1].set_xlabel("matrix size")
+    axs[1].set_ylabel("Time (s)")
+
+    plt.tight_layout()
+    plt.savefig("timings.png", dpi=300)
 
 # Problem 4
 def prob4():
@@ -122,4 +199,78 @@ def prob4():
     four sets of execution times on a regular linear scale, and one with all
     four sets of exections times on a log-log scale.
     """
-    raise NotImplementedError("Problem 4 Incomplete")
+    #gonna do times
+    ns = [2**2, 2**4, 2**6, 2**8]
+    vec_times = []
+    mat_times = []
+    np_vec = []
+    np_mat = []
+    for n in ns:
+        A = random_matrix(n)
+        x = random_vector(n)
+        B = random_matrix(n)
+
+        start = time.time()
+        matrix_vector_product(A, x)
+        vec_time = (time.time() - start)
+        vec_times.append(vec_time)
+       
+        start_mat = time.time()
+        matrix_matrix_product(A, B)
+        mat_time = (time.time() - start_mat)
+        mat_times.append(mat_time)
+
+
+        start_np = time.time()
+        np.dot(A, x)
+        np_dot_vec_time = (time.time() - start_np)
+        np_vec.append(np_dot_vec_time)
+
+        start_np_mat = time.time()
+        np.dot(A, B)
+        np_dot_mat_time = (time.time() - start_np_mat)
+        np_mat.append(np_dot_mat_time)
+    #plot the results
+    fig, axs = plt.subplots(1, 2, figsize=(12, 5))  
+
+    axs[0].plot(ns, vec_times, marker='o', label='Python mat-vec')
+    axs[0].plot(ns, mat_times, marker='o', color='orange', label='Python mat-mat')
+    axs[0].plot(ns, np_vec, marker='o', color='red', label='NumPy mat-vec')
+    axs[0].plot(ns, np_mat, marker='o', color='green', label='NumPy mat-mat')
+    axs[0].set_title("lin-lin Plot")
+    axs[0].set_xlabel("Matrix Size")
+    axs[0].set_ylabel("Seconds")
+    axs[0].legend()
+
+    
+    axs[1].loglog(ns, vec_times, marker='o', label='Python mat-vec')
+    axs[1].loglog(ns, mat_times, marker='o', color='orange', label='Python mat-mat')
+    axs[1].loglog(ns, np_vec, marker='o', color='red', label='NumPy mat-vec')
+    axs[1].loglog(ns, np_mat, marker='o', color='green', label='NumPy mat-mat')
+    axs[1].set_title("log-log Plot")
+    axs[1].set_xlabel("Matrix Size")
+    axs[1].set_ylabel("Seconds")
+    axs[1].legend()
+
+    plt.tight_layout()
+    plt.savefig("linlinvsloglogtimings.png", dpi=300)
+
+
+
+
+
+# if __name__ == "__main__":
+#     solar_system(T = 3*np.pi/2, x_e = 10, x_m = 11, omega_e = 1, omega_m = 13)
+#     prob3()
+#     prob4()
+
+    
+#     solar_system(
+#     T=3*np.pi/2,  # final time
+#     x_e=10,       # Earth’s initial x position
+#     x_m=11,       # Moon’s initial x position
+#     omega_e=1,    # Earth angular velocity
+#     omega_m=13    # Moon angular velocity
+# )
+#     prob4()
+
